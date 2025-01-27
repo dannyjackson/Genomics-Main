7e7e535027a6563b6b19006c6572c2015f49ba26
Danny Jackson <dnjacks4@asu.edu>
2025-01-27 16:55:29 -0700

    Update fst_params.sh

--- fst_params.sh
+++ fst_params.sh
@@ -1,7 +1,6 @@
 module load R/4.4.0
 module load htslib/1.19.1
 module load bedtools2/2.29.2
-module load samtools/1.19.2
 
 MSMCTOOLS=~/programs/msmc-tools # directory with msmc-tools binaries
 PATH=$PATH:$MSMCTOOLS
@@ -36,5 +35,4 @@ CHRLEAD=NC_0
 REF=/xdisk/mcnew/dannyjackson/cardinals_dfinch/datafiles/referencegenome/ncbi_dataset/data/GCF_901933205.1/GCF_901933205.1_STF_HiC_genomic.fna
 # path to gff file
 GFF=/xdisk/mcnew/dannyjackson/cardinals_dfinch/datafiles/referencegenome/ncbi_dataset/data/GCA_013397215.1/genomic.gff 
-
 
