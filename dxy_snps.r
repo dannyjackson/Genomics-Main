f41dc7a0f8089e0af6362481e7e1d3893ff68f51
Danny Jackson <dnjacks4@asu.edu>
2025-01-29 16:13:10 -0700

    Update dxy_snps.r

--- dxy_snps.r
+++ dxy_snps.r
@@ -69,9 +69,7 @@ ggplot(plot_data, aes(x = BPcum, y = dxy)) +
   scale_x_continuous(labels = axisdf$chromo, breaks = axisdf$center, guide = guide_axis(n.dodge = 2)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +
   labs(x = "Chromosome", y = "dxy") +
-  geom_hline(yintercept = dxy_cutoff) +
-  geom_label_repel(data = plot_data %>% filter(is_annotate == "yes"), 
-                   aes(label = as.factor(position)), size = 5, force = 1.3, alpha = 0.7) +
+  geom_hline(yintercept = dxy_cutoff) ++
   theme_bw(base_size = 22) +
   theme(
     plot.title = element_text(hjust = 0.5),
