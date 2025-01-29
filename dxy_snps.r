78af1bc0ff487318cff318cdc6272999f4b29ccf
Danny Jackson <dnjacks4@asu.edu>
2025-01-29 15:48:23 -0700

    Update dxy_snps.r

--- dxy_snps.r
+++ dxy_snps.r
@@ -1,5 +1,16 @@
 #!/usr/bin/env Rscript
+# Load required packages, installing if necessary
+required_packages <- c("qqman", "readr", "ggrepel", "ggplot2", "dplyr", "RColorBrewer")
+installed_packages <- rownames(installed.packages())
 
+for (pkg in required_packages) {
+  if (!(pkg %in% installed_packages)) {
+    install.packages(pkg, repos = "http://cran.us.r-project.org")
+  }
+  library(pkg, character.only = TRUE)
+}
+
+# Parse command-line arguments
 args <- commandArgs(trailingOnly = TRUE)
 outdir <- args[1]
 pop1 <- args[2]
@@ -7,129 +18,68 @@ pop2 <- args[3]
 color1 <- args[4]
 color2 <- args[5]
 
-# Package names
-packages <- c("qqman", "readr", "ggrepel", "ggplot2", "dplyr", "RColorBrewer")
+# Read and clean data
+dxy_file <- file.path(outdir, "analyses/dxy", paste0(pop1, "_", pop2, "/Dxy_persite_nocaurban_nocarural.txt"))
+dxy <- read.csv(dxy_file, sep = '\t') %>% na.omit()
 
-# Install packages not yet installed
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
-}
-
-# Packages loading
-invisible(lapply(packages, library, character.only = TRUE))
-
-
-
-# Plot dxy 
-
-dxy <- read.csv(paste0(outdir, "/analyses/dxy/", pop1, "_", pop2,'/Dxy_persite_nocaurban_nocarural.txt'), sep ='\t')
-
-
-dxy_no_na <- na.omit(dxy)
-nrow(dxy) - nrow(dxy_no_na) 
-dxy <- dxy_no_na
-
-# z transform dxy values
-
+# Z-transform dxy values
 dxy_xbar <- mean(dxy$dxy, na.rm = TRUE)
 dxy_sd <- sd(dxy$dxy, na.rm = TRUE)
-
 dxy$z <- (dxy$dxy - dxy_xbar) / dxy_sd
+dxy$neg_log_pvalues_one_tailed <- -log10(pnorm(dxy$z, lower.tail = FALSE))
 
-p_values_one_tailed <- pnorm(q = dxy$z, lower.tail = FALSE)
-
-# Calculate the -log10 of the p-value
-dxy$neg_log_pvalues_one_tailed <- -log10(p_values_one_tailed)
-
-ordered_dxy <- dxy %>% 
- # desc orders from largest to smallest
- arrange(desc(neg_log_pvalues_one_tailed)) 
-
+# Identify top outliers
 nsnps <- nrow(dxy)
 top_snps <- round(nsnps * 0.001)
+outlier_dxy <- dxy %>% 
+  arrange(desc(neg_log_pvalues_one_tailed)) %>% 
+  head(top_snps) %>% 
+  arrange(chromo, position)
 
-outlier_dxy_disorder <- ordered_dxy[1:top_snps, ]
+dxy_cutoff <- min(outlier_dxy$dxy)
 
-outlier_dxy <- outlier_dxy_disorder %>% arrange(chromo, position)
+# Save cutoff value
+cutoff_file <- file.path(outdir, "analyses/dxy", paste0(pop1, "_", pop2, "dxy_stats.txt"))
+writeLines(c("dxy cutoff snps:", dxy_cutoff), cutoff_file, append = TRUE)
 
-dxy_cutoff <- min(outlier_dxy_disorder$dxy) # print to file
+# Save outliers
+outlier_file <- file.path(outdir, "analyses/dxy", paste0(pop1, "_", pop2, "/", pop1, "_", pop2, ".chrom.dxy.snps.outlierdxy.csv"))
+write.csv(outlier_dxy, outlier_file, row.names = FALSE)
 
-
-cat(c("dxy cutoff snps:", dxy_cutoff),
-    file = paste0(outdir, "/analyses/dxy/", pop1, "_", pop2, "dxy_stats.txt"),
-    sep = "\n",
-    append = TRUE)
-
-
-write.csv(outlier_dxy,
-          paste0(outdir, "/analyses/dxy/",
-                 pop1, "_", pop2, "/", pop1, "_", pop2, ".chrom.dxy.snps.outlierdxy.csv"))
-
-
-# draw it with cutoff line 
-
-
-colors <- c(color1, color2)
-
+# Prepare data for plotting
 dxy$chromo <- factor(dxy$chromo, levels = c(1, "1A", 2:4, "4A", 5:29, "Z"))
 
-
-df.tmp <- dxy %>%
-
-  # Compute chromosome size
+plot_data <- dxy %>%
   group_by(chromo) %>%
   summarise(chr_len = max(position)) %>%
-
-  # Calculate cumulative position of each chromosome
   mutate(tot = cumsum(chr_len) - chr_len) %>%
   select(-chr_len) %>%
-
-  # Add this info to the initial dataset
-  left_join(dxy, ., by = c("chromo" = "chromo")) %>%
-
-  # Add a cumulative position of each SNP
+  left_join(dxy, by = "chromo") %>%
   arrange(chromo, position) %>%
   mutate(BPcum = position + tot)
 
-half_length <- ceiling(length(unique(dxy$chromo)) / 2)
-
-# get chromosome center positions for x-axis
-axisdf <- df.tmp %>%
+axisdf <- plot_data %>%
   group_by(chromo) %>%
-  summarize(center = (max(BPcum) + min(BPcum)) / 2)
+  summarize(center = mean(BPcum))
 
-png(file = paste0(outdir, "/analyses/dxy/",
-                   pop1, "_", pop2, "/", pop1, "_", pop2, ".dxy.snps.sigline.png"),
-    width = 2000, height = 500)
-
-ggplot(df.tmp, aes(x = BPcum, y = (dxy))) +
-  # Show all points
+# Plot
+ggplot(plot_data, aes(x = BPcum, y = dxy)) +
   geom_point(aes(color = as.factor(chromo)), alpha = 0.8, size = 1) +
-  scale_color_manual(values = rep(colors, half_length)) +
-  # custom X axis:
-  # expand=c(0,0)removes space between plot area and x axis
-    scale_x_continuous( label = axisdf$chromo, breaks= axisdf$center, guide = guide_axis(n.dodge = 2) ) +
-
-  scale_y_continuous(expand <- c(0, 0), limits <- c(0, 1)) +
-  # add plot and axis titles
-  ggtitle(NULL) +
+  scale_color_manual(values = rep(c(color1, color2), length(unique(dxy$chromo)) / 2)) +
+  scale_x_continuous(labels = axisdf$chromo, breaks = axisdf$center, guide = guide_axis(n.dodge = 2)) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 1)) +
   labs(x = "Chromosome", y = "dxy") +
-  # add genome-wide sig and sugg lines
   geom_hline(yintercept = dxy_cutoff) +
-  # Add highlighted points
-  #geom_point(data=subset(df.tmp, is_highlight=="yes"),color="orange", size=2) +
-  # Add label using ggrepel to avoid overlapping
-  geom_label_repel(data=df.tmp[df.tmp$is_annotate=="yes",], aes(label=as.factor(position), alpha=0.7), size=5, force=1.3) +
-  # Custom the theme:
-  theme_bw(base_size <- 22) +
+  geom_label_repel(data = plot_data %>% filter(is_annotate == "yes"), 
+                   aes(label = as.factor(position)), size = 5, force = 1.3, alpha = 0.7) +
+  theme_bw(base_size = 22) +
   theme(
-    plot.title <- element_text(hjust <- 0.5),
+    plot.title = element_text(hjust = 0.5),
     legend.position = "none",
-    panel.border <- element_blank(),
-    panel.grid.major.x <- element_blank(),
-    panel.grid.minor.x <- element_blank()
+    panel.border = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
   )
 
-
-dev.off()
+ggsave(filename = file.path(outdir, "analyses/dxy", paste0(pop1, "_", pop2, "/", pop1, "_", pop2, ".dxy.snps.sigline.png")), 
+       width = 20, height = 5, units = "in")
