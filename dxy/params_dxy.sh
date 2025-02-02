d025d2e3bda4d1079a5083eba1bf3d8ac763b077
Danny Jackson <dnjacks4@asu.edu>
2025-02-01 17:20:07 -0700

    splitting params_dxy into params and setup_dxy

--- dxy/params_dxy.sh
+++ dxy/params_dxy.sh
@@ -13,11 +13,5 @@ COLOR2=#XXXXXX
 POP1=population1name
 POP2=population2name
 
-# make directories for intermediate files-- will fail if these don't exist
-
-mkdir -p ${OUTDIR}/analyses/{dxy,genelist}
-mkdir -p ${OUTDIR}/datafiles/{safs,mls}
-mkdir -p ${OUTDIR}/analyses/genelist/${POP1}_${POP2}/
-mkdir -p ${OUTDIR}/analyses/genelist/${POP1}_${POP2}/${WIN}
-mkdir -p ${OUTDIR}/analyses/dxy/${POP1}_${POP2}
-mkdir -p ${OUTDIR}/analyses/dxy/${POP1}_${POP2}/${WIN}
+# source the setup file for dxy                                           
+source ${SCRIPTDIR}/Genomics-Main/setup_dxy.sh
