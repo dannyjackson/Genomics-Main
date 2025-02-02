676e32302ed401655ff7b998ef45084d1e69abf9
Danny Jackson <dnjacks4@asu.edu>
2025-02-01 17:40:57 -0700

    Update dxy_1.sh

--- dxy/dxy_1.sh
+++ dxy/dxy_1.sh
@@ -115,8 +115,8 @@ awk 'NR>1 {print $2}' "${OUTDIR}/analyses/dxy/${POP1}_${POP2}/Dxy_persite_${POP1
 
 
 # Run visualization scripts
-Rscript ${SCRIPTDIR}/Genomics-Main/dxy_snps.r "${OUTDIR}" "${POP1}" "${POP2}" "${COLOR1}" "${COLOR2}" "${CUTOFF}
+Rscript ${SCRIPTDIR}/Genomics-Main/dxy/dxy_snps.r "${OUTDIR}" "${POP1}" "${POP2}" "${COLOR1}" "${COLOR2}" "${CUTOFF}
 
-python ${SCRIPTDIR}/Genomics-Main/dxy_windows.py --outdir "${OUTDIR}" --pop1 "${POP1}" --pop2 "${POP2}" --win "${WIN}" 
+python ${SCRIPTDIR}/Genomics-Main/dxy/dxy_windows.py --outdir "${OUTDIR}" --pop1 "${POP1}" --pop2 "${POP2}" --win "${WIN}" 
 
-Rscript ${SCRIPTDIR}/Genomics-Main/dxy_windows.r "${OUTDIR}" "${POP1}" "${POP2}" "${COLOR1}" "${COLOR2}" "${CUTOFF}
+Rscript ${SCRIPTDIR}/Genomics-Main/dxy/dxy_windows.r "${OUTDIR}" "${POP1}" "${POP2}" "${COLOR1}" "${COLOR2}" "${CUTOFF}
