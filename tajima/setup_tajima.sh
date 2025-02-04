34f983779f2f6f67d3345ad501b0afd5f0c462e7
Danny Jackson <dnjacks4@asu.edu>
2025-02-04 06:39:02 -0700

    outputting all tajima files to thetas directory instead

--- tajima/setup_tajima.sh
+++ tajima/setup_tajima.sh
@@ -1,9 +1,9 @@
 # make directories for intermediate files-- will fail if these don't exist
 
-mkdir -p ${OUTDIR}/analyses/{tajima,genelist,thetas}
+mkdir -p ${OUTDIR}/analyses/{genelist,thetas}
 mkdir -p ${OUTDIR}/datafiles/{safs,mls}
 mkdir -p ${OUTDIR}/analyses/genelist/${POP}/
 mkdir -p ${OUTDIR}/analyses/genelist/${POP}/${WIN}
-mkdir -p ${OUTDIR}/analyses/tajima/${POP}
-mkdir -p ${OUTDIR}/analyses/tajima/${POP}/${WIN}
-mkdir -p ${OUTDIR}/analyses/tajima/${POP}/snps
+mkdir -p ${OUTDIR}/analyses/thetas/${POP}
+mkdir -p ${OUTDIR}/analyses/thetas/${POP}/${WIN}
+mkdir -p ${OUTDIR}/analyses/thetas/${POP}/snps
