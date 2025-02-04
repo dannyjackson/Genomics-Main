da86121a0b436818cce9ece9b9473ea6f0f68887
Danny Jackson <dnjacks4@asu.edu>
2025-02-04 09:22:45 -0700

    Update setup_tajima.sh

--- tajima/setup_tajima.sh
+++ tajima/setup_tajima.sh
@@ -7,3 +7,6 @@ mkdir -p ${OUTDIR}/analyses/genelist/${POP}/${WIN}
 mkdir -p ${OUTDIR}/analyses/thetas/${POP}
 mkdir -p ${OUTDIR}/analyses/thetas/${POP}/${WIN}
 mkdir -p ${OUTDIR}/analyses/thetas/${POP}/snps
+mkdir -p ${OUTDIR}/analyses/Tajima/
+mkdir -p ${OUTDIR}/analyses/Tajima/${POP}
+
