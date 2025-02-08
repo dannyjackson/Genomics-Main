6af0fc2d8097ffdff1e2be2bc6ee50a160cd6333
Danny Jackson <dnjacks4@asu.edu>
2025-02-07 18:17:20 -0700

    Update outlier_to_bed.py

--- general_scripts/outlier_to_bed.py
+++ general_scripts/outlier_to_bed.py
@@ -15,11 +15,6 @@ df['stop'] = df['position'] + (window // 2) if window else df['position']
 # Create a BED file DataFrame
 bed_df = df[['chromo', 'start', 'stop']]
 
-bed_df['chromo'] = bed_df['chromo'].astype(float)
-
-# Sort numerically by chromosome number
-bed_df = bed_df.sort_values(by='chromo')
-
 # Save to a BED file
 bed_df.to_csv("output.bed", sep='\t', header=False, index=False)
 
