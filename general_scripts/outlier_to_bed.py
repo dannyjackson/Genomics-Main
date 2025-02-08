c4df2bd6455b4312519c87afeccfa7af99eaa5bb
Danny Jackson <dnjacks4@asu.edu>
2025-02-07 18:04:54 -0700

    Create outlier_to_bed.py

---
+++ general_scripts/outlier_to_bed.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sys
+
+# Read input data and optional window size from command-line arguments
+input_data = sys.argv[1]
+window = int(sys.argv[2]) if len(sys.argv) > 2 else None
+
+# Read the data into a DataFrame
+df = pd.read_csv(pd.compat.StringIO(input_data))
+
+# Compute start and stop positions based on window size
+df['start'] = df['position'] - (window // 2) if window else df['position'] - 1
+df['stop'] = df['position'] + (window // 2) if window else df['position']
+
+# Create a BED file DataFrame
+bed_df = df[['chromo', 'start', 'stop']]
+bed_df['chromo'] = 'chr' + bed_df['chromo'].astype(str)
+
+# Save to a BED file
+bed_df.to_csv("output.bed", sep='\t', header=False, index=False)
+
+print("BED file created successfully.")
