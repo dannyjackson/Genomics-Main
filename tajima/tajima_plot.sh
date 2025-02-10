06900c588d788c64a517c75f36f693ff4f38c5eb
Danny Jackson <dnjacks4@asu.edu>
2025-02-10 08:51:04 -0700

    Update tajima_plot.sh

--- tajima/tajima_plot.sh
+++ tajima/tajima_plot.sh
@@ -7,35 +7,25 @@ if [ $# -lt 1 ]; then
     cat <<EOF
 Usage: $(basename "$0") -p <parameter_file> [-w <window_size>] [-s <step_size>] [-c <chrom_file>]
 
-This script computes Tajima's D within a population of genomes using a genotype likelihood framework implemented in ANGSD.
-
-It requires SAF files as input, which can be generated using the scripts available at:
-    https://github.com/dannyjackson/Genomics-Main
-
-It computes:
-    - Genome-wide average Tajima's D
-    - Sliding window Tajima's D
-    - Per-SNP Tajima's D
+This script plots the output of a z-transformed windowed analysis of various metrics (dxy, fst, Tajima's D, etc.)
 
 Ensure you read and understand the entire script before running it.
 
 REQUIRED ARGUMENTS:
     -p  Path to the parameter file (e.g., params_dxy.sh)
-OPTIONAL ARGUMENTS:
     -w  Window size for sliding window analysis 
-    -s  Step size for sliding window analysis
-    -c  Path to a chromosome mapping file (optional)
+    -m  Name of metric (dxy, fst, Tajima)
 EOF
     exit 1
 fi
 
 
 # Parse command-line arguments
-while getopts "p:w:s:c:" option; do
+while getopts "p:w:c:" option; do
     case "${option}" in
         p) PARAMS=${OPTARG} ;;
         w) WIN=${OPTARG} ;;
-        s) STEP=${OPTARG} ;;
+        m) METRIC=${OPTARG} ;;
         *) echo "Error: Invalid option '-${OPTARG}'" >&2; exit 1 ;;
     esac
 done
@@ -53,12 +43,13 @@ fi
 source "${PARAMS}"
 
 # Ensure required variables are set
-if [ -z "${OUTDIR}" ] || [ -z "${POP}" ] || [ -z "${WIN}" ] || [ -z "${STEP}" ]; then
-    echo "Error: OUTDIR, POP, WIN, or STEP is not set in the parameter file." >&2
+if [ -z "${OUTDIR}" ] || [ -z "${POP}" ] || [ -z "${WIN}" ] || [ -z "${METRIC}" ]; then
+    echo "Error: OUTDIR, POP, WIN, or METRIC is not set in the parameter file." >&2
     exit 1
 fi
 
-WIN_OUT="${OUTDIR}/analyses/thetas/${POP}/${WIN}/${POP}.theta.thetasWindow.pestPG"
+
+WIN_OUT="${OUTDIR}/analyses/${METRIC}/${POP}/${POP}.${METRIC}_"${WIN}.Ztransformed.csv"
 
 # Run R script for plotting
 echo "Generating Manhattan plot from ${WIN_OUT}..."
