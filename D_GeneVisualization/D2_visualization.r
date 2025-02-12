4bb45f7991f2a035a284dd04e389ea9d9aeb0af2
Danny Jackson <dnjacks4@asu.edu>
2025-02-12 13:14:07 -0700

    update

--- D_GeneVisualization/D2_visualization.r
+++ D_GeneVisualization/D2_visualization.r
@@ -1,7 +1,28 @@
-# BiocManager::install("org.Gg.eg.db")
-library(org.Gg.eg.db)
-library(limma)
-library(rrvgo)
+#!/usr/bin/env Rscript
+
+# Load required packages, installing if necessary
+BiocManager::install("org.Gg.eg.db")
+
+required_packages <- c("org.Gg.eg.db", "limma", "rrvgo")
+installed_packages <- rownames(installed.packages())
+
+cat("Checking required packages...\n")
+for (pkg in required_packages) {
+  if (!(pkg %in% installed_packages)) {
+    install.packages(pkg, repos = "http://cran.us.r-project.org")
+  }
+  library(pkg, character.only = TRUE)
+}
+
+cat("Parsing command-line arguments...\n")
+# Parse command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+outdir <- args[1]
+scriptdir <- args [2]
+file <- args[3]
+
+infile <- file.path(scriptdir, "D_CandidateGeneAnalyses/GOterms", $file, ".txt")
+
 
 # common genes in both
 
@@ -18,11 +39,18 @@ reducedTerms <- reduceSimMatrix(simMatrix,
                                 orgdb="org.Gg.eg.db")
 
 
+outfile <- file.path(outdir, "analyses/genelists/plots", paste0($file, ".scatter.png"))
+png("scatterplot.png", width = 800, height = 600)  
 scatterPlot(simMatrix, reducedTerms)
+dev.off()
 
+outfile <- file.path(outdir, "analyses/genelists/plots", paste0($file, ".treemap.png"))
 treemapPlot(reducedTerms)
+dev.off()
 
-
+outfile <- file.path(outdir, "analyses/genelists/plots", paste0($file, ".heatmap.png"))
 heatmapPlot(simMatrix,
             annotateParent=TRUE,
             fontsize=6)
+dev.off()
+
