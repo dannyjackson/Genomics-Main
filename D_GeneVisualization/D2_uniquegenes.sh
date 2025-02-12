aea074b54781af4a1f6741839cd6ab29626ee70b
Danny Jackson <dnjacks4@asu.edu>
2025-02-12 11:39:56 -0700

    Update D2_uniquegenes.sh

--- D_GeneVisualization/D2_uniquegenes.sh
+++ D_GeneVisualization/D2_uniquegenes.sh
@@ -10,7 +10,8 @@ This script creates a gene list given a list of sites in a genome and a gff file
 Ensure you read and understand the entire script before running it.
 
 REQUIRED ARGUMENTS:
-    -p  Name of the population of interest
+    -p  Path to parameter file
+    -i  Name of the population of interest
     -q  Name of the control population (removes genes identified in this pop from the final file)
     -m  Metric (e.g. Tajima, raisd)
     -w  Window size 
@@ -31,5 +32,8 @@ while getopts "p:i:q:m:w:" option; do
     esac
 done
 
-source ${PARAMS}
-grep -Fxv -f ${OUTDIR}/analyses/genelist/gene_names/${POP2}.${METRIC}.${WIN}.genenames.txt ${OUTDIR}/analyses/genelist/gene_names/${POP1}.${METRIC}.${WIN}.genenames.txt > ${OUTDIR}/analyses/genelist/final_gene_lists/${POP1}.${METRIC}.${WIN}.unique.genenames.txt
+source "$PARAMS"
+grep -Fxv -f "${OUTDIR}/analyses/genelist/gene_names/${POP2}.${METRIC}.${WIN}.genenames.txt" \
+            "${OUTDIR}/analyses/genelist/gene_names/${POP1}.${METRIC}.${WIN}.genenames.txt" > \
+            "${OUTDIR}/analyses/genelist/final_gene_lists/${POP1}.${METRIC}.${WIN}.unique.genenames.txt"
+
