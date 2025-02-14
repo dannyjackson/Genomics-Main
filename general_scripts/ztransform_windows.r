48ee7376ca10c7b799212a2aa093e59b01936441
Danny Jackson <dnjacks4@asu.edu>
2025-02-14 12:36:55 -0700

    Update ztransform_windows.r

--- general_scripts/ztransform_windows.r
+++ general_scripts/ztransform_windows.r
@@ -82,4 +82,4 @@ df <- data[ -c(1) ]
 # save file
 cat("Saving Z-transformed data...\n")
 z_file <- file.path(outdir, "analyses", metric, paste0(pop_name, "/", pop_name, ".", metric, ".", win, ".Ztransformed.csv"))
-write_delim(df, z_file, quote = FALSE, row.names = FALSE, sep = "\t")
+write_delim(df, z_file, quote = none, delim = "\t")
