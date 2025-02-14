937ea3a73c51d2c6200faa5c62cdf47f9cf3c8b3
Danny Jackson <dnjacks4@asu.edu>
2025-02-14 10:55:14 -0700

    troubleshooting

--- C_SelectionAnalysis/fst/fst.sh
+++ C_SelectionAnalysis/fst/fst.sh
@@ -107,7 +107,8 @@ Rscript "${SCRIPTDIR}/Genomics-Main/general_scripts/ztransform_windows.r" \
     "${OUTDIR}" "${CUTOFF}" "${WIN_OUT}" "${WIN}" "${POP1}_${POP2}"
 
 Z_OUT="${OUTDIR}/analyses/fst/${POP1}_${POP2}/${POP1}_${POP2}.fst.${WIN}.Ztransformed.csv"
-# sed -i 's/\"//g' ${Z_OUT}
+
+sed -i 's/\"//g' ${Z_OUT}
 
 # Run R script for plotting
 echo "Generating Manhattan plot from ${Z_OUT}..."
