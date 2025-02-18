48e147bb8cbd7a916ec030d4d40eb7051be92d66
Danny Jackson <dnjacks4@asu.edu>
2025-02-17 17:04:28 -0700

    Update SNPeff_working.sh

--- D_GeneVisualization/SNPeff_working.sh
+++ D_GeneVisualization/SNPeff_working.sh
@@ -12,6 +12,16 @@ gunzip jdk-23_linux-x64_bin.tar.gz
 # download snpeff databases for chicken and great tit
 wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
 
+git clone https://github.com/pcingola/SnpEff.git
+module load maven/3.6.3
+
+mvn install:install-file \
+    -Dfile=biojava3-core-3.0.7.jar \
+    -DgroupId=org.biojava \
+    -DartifactId=biojava3-core \
+    -Dversion=3.0.7 \
+    -Dpackaging=jar
+
 # Annotate SNPs in SnpEff (Cingolani et al. 2012)
 # check if prebuilt genome exists in database
 ~/programs/jdk-23.0.2/bin/java -jar snpEff.jar databases | grep -i 'Camarhynchus'
@@ -39,79 +49,171 @@ cp /xdisk/mcnew/dannyjackson/cardinals/datafiles/referencegenome/ncbi_dataset/da
 cp /xdisk/mcnew/dannyjackson/cardinals/datafiles/referencegenome/ncbi_dataset/data/GCF_901933205.1/protein.faa ./protein.fa
 cp /xdisk/mcnew/dannyjackson/cardinals/datafiles/referencegenome/ncbi_dataset/data/GCF_901933205.1/cds_from_genomic.fna ./cds.fa
 
-cd /home/u15/dannyjackson/programs/snpEff/
 
 sed -i '$ d' snpEff.config
 
 echo 'GCA_901933205.genome : GCA_901933205' >> snpEff.config
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gff3 -v GCA_901933205
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -genbank GCF_901933205
+
+~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gff3 -v GCA_901933205 2>&1 | tee GCA_901933205.build.output
+
+sed -i 's/java/\~\/programs\/jdk\-23\.0\.2\/bin\/java/g' /home/u15/dannyjackson/programs/snpEff/scripts/buildDbNcbi.sh 
+
+sed -i 's/jdk\-2/jdk\-23/g' /home/u15/dannyjackson/programs/snpEff/scripts/buildDbNcbi.sh 
+
+sed -i '$ d' snpEff.config
+
+/home/u15/dannyjackson/programs/snpEff/scripts/buildDbNcbi.sh GCF_901933205.1
+
+FATAL ERROR: No CDS checked. This might be caused by differences in FASTA file transcript IDs respect to database's transcript's IDs.
+Transcript IDs from database (sample):
+        'XM_030965427.1'
+        'rna-XM_030956335.1'
+        'XM_030965425.1'
+        'XM_030965424.1'
+
+Transcript IDs from database (fasta file):
+        'lcl|NC_044572.1_cds_XP_030823689.1_2603'
+        'lcl|NC_044586.1_cds_XP_030815405.1_19420'
+        'lcl|NC_044571.1_cds_XP_030801370.1_1690'
+        'lcl|NC_044575.1_cds_XP_030805537.1_8939'
+
+
+# fix cds
+import sys
+
+def restructure_fasta(input_file, output_file):
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in infile:
+            if line.startswith('>'):
+                parts = line.split()
+                protein_id = None
+                for part in parts:
+                    if part.startswith("[protein_id="):
+                        protein_id = part.split("=")[1].rstrip("]")
+                        break
+                if protein_id:
+                    new_header = f">{protein_id} {line[1:]}"
+                    outfile.write(new_header)
+                else:
+                    outfile.write(line)
+            else:
+                outfile.write(line)
+
+if __name__ == "__main__":
+    input_fasta = "cds.fa"  # Change to your input file name
+    output_fasta = "cds.revised.fa"  # Change to your desired output file name
+    restructure_fasta(input_fasta, output_fasta)
+
+
+
+# this improved the situation but I am now getting this error:
+FATAL ERROR: No CDS checked. This might be caused by differences in FASTA file transcript IDs respect to database's transcript's IDs.
+Transcript IDs from database (sample):
+        'XM_030965427.1'
+        'rna-XM_030956335.1'
+        'XM_030965425.1'
+        'XM_030965424.1'
+        'rna-XR_004060613.1'
+        'rna-XM_030952858.1'
+Transcript IDs from database (fasta file):
+        'XP_030820196.1'
+        'XP_030822353.1'
+        'XP_030800543.1'
+
+
+# fix gtf
+import re
+
+def replace_transcript_with_protein(gtf_file, output_file):
+    # Open the input GTF file and the output file
+    with open(gtf_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in infile:
+            # Skip comment lines
+            if line.startswith("#") or line.startswith("!"):
+                outfile.write(line)
+                continue
+
+            # Match and extract transcript_id and protein_id
+            transcript_match = re.search(r'transcript_id\s+"([^"]+)"', line)
+            protein_match = re.search(r'protein_id\s+"([^"]+)"', line)
+
+            # If both transcript_id and protein_id are found, replace transcript_id with protein_id
+            if transcript_match and protein_match:
+                transcript_id = transcript_match.group(1)
+                protein_id = protein_match.group(1)
+                # Replace transcript_id with protein_id in the line
+                line = line.replace(f'transcript_id "{transcript_id}"', f'transcript_id "{protein_id}"')
+
+            # Write the modified (or unmodified) line to the output file
+            outfile.write(line)
+
+# Example usage
+input_gtf_file = '../genes.gtf'
+output_gtf_file = 'genes.gtf'
+replace_transcript_with_protein(input_gtf_file, output_gtf_file)
+
+
+
+~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gff3 -v GCA_901933205 2>&1 | tee GCA_901933205.build.output
+~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gff2 -v GCA_901933205 2>&1 | tee GCA_901933205.build.output
+
+cd /home/u15/dannyjackson/programs/snpEff
+~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gtf22 -v GCA_901933205 2>&1 | tee GCA_901933205.build.output
+# broke on memory
+
+interactive -a mcnew -m 50gb
+~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gtf22 -v GCA_901933205 2>&1 | tee GCA_901933205.build.output
+
+
+
+
+
+WARNING_TRANSCRIPT_NOT_FOUND: Cannot find transcript 'XM_030955085.1'. Created transcript 'XM_030955085.1' and gene 'LOC115907285' for NC_044571.1      Gnomon  EXON    1693    3360
+    -
+        db_xref : GeneID:115907285
+        exon_number : 1
+        gbkey : mRNA
+        gene : LOC115907285
+        gene_id : LOC115907285
+        model_evidence : Supporting evidence includes similarity to: 95% coverage of the annotated genomic feature by RNAseq alignments, including 3 samples with support for all annotated introns
+        product : uncharacterized LOC115907285
+        transcript_id : XM_030955085.1
+. File '/home/u15/dannyjackson/programs/snpEff/./data/GCA_901933205/genes.gtf' line 6   'NC_044571.1    Gnomon  exon    1694    3361    .       -       .       gene_id "LOC115907285"; transcript_id "XM_030955085.1"; db_xref "GeneID:115907285"; gbkey "mRNA"; gene "LOC115907285"; model_evidence "Supporting evidence includes similarity to: 95% coverage of the annotated genomic feature by RNAseq alignments, including 3 samples with support for all annotated introns"; product "uncharacterized LOC115907285"; exon_number "1"; '
+
+
+# jfc that was awful but now it works
+# i don't know what's up with these transcript issues but I'm choosing to ignore it
 
 # annotate all snps in vcf
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar -c /home/u15/dannyjackson/programs/snpEff/snpEff.config -v GCA_901933205 /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_snps_multiallelic.vcf > /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_snps_multiallelic.snpEffann.vcf 
 
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gff3 -v ncbi_cardinalis 
-~/programs/jdk-23.0.2/bin/java -Xmx4g -jar /path/to/snpEff/snpEff.jar -c /path/to/snpEff/snpEff.config -v b10k VYXE01005221.vcf > VYXE01005221.an…
-https://snpeff.blob.core.windows.net/databases/v5_1/snpEff_v5_1_b10k.zip
+#!/bin/sh
 
+~/programs/jdk-23.0.2/bin/java -jar /home/u15/dannyjackson/programs/snpEff/snpEff.jar -c /home/u15/dannyjackson/programs/snpEff/snpEff.config -v GCA_901933205 /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_snps_multiallelic.vcf > /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_snps_multiallelic.snpEffann.vcf 
 
+grep 'missense_variant' /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_snps_multiallelic.snpEffann.vcf 
 
+sbatch --account=mcnew \
+        --job-name=snpeff \
+        --partition=standard \
+        --mail-type=ALL \
+        --output=slurm_output/output.snpeff.%j \
+        --nodes=1 \
+        --ntasks-per-node=4 \
+        --time=1:00:00 \
+        --mem=50gb \
+        /xdisk/mcnew/dannyjackson/cardinals/scripts/snpeff_wholegenome.sh
 
+#!/bin/sh
 
+~/programs/jdk-23.0.2/bin/java -jar /home/u15/dannyjackson/programs/snpEff/snpEff.jar -c /home/u15/dannyjackson/programs/snpEff/snpEff.config -v GCA_901933205 /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_filtered.recode.vcf > /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_filtered.snpEffann.vcf 
 
-
-
-
-
-
-# I need to make a vcf of outlier snps
-# in this file, $2 is chromo and $3 is position
-# /xdisk/mcnew/dannyjackson/cardinals/analyses/fst/nocaurban_nocarural/nocaurban_nocarural.fst.25000.Ztransformed.csv
-# I need to convert chromosome names in the output file to new chrom names 
-
-CHR_FILE="/xdisk/mcnew/dannyjackson/cardinals/referencelists/GCF_901933205_chromconversion.txt"
-FILE="/xdisk/mcnew/dannyjackson/cardinals/analyses/fst/nocaurban_nocarural/nocaurban_nocarural.fst.25000.Ztransformed.csv"
-OUT_FILE="/xdisk/mcnew/dannyjackson/cardinals/analyses/fst/nocaurban_nocarural/nocaurban_nocarural.fst.25000.Ztransformed.chrnames.csv"
-
-# Process the CSV file, replacing only the second column
-awk -F',' -v CHR_FILE="$CHR_FILE" '
-BEGIN {
-    OFS = "\t";  # Set output field separator to tab
-    while ((getline < CHR_FILE) > 0) {
-        map[$1] = $2;  # Store mappings from second column to first column
-    }
-}
-BEGIN { FS = "\t" }  # Set input field separator to tab for FILE processing
-{
-    if ($2 in map) {
-        $2 = map[$2];  # Replace second column if match found
-    }
-    print;
-}' "$FILE" > "$OUT_FILE"
-
-echo "Finished processing. Output saved to $OUT_FILE"
-
-# make a regions file from this
-# CHROM, BEG, END
-# given a file with chromo in $2 and position in $3,
-awk '{print $2, $3}' /xdisk/mcnew/dannyjackson/cardinals/analyses/fst/nocaurban_nocarural/nocaurban_nocarural.fst.25000.Ztransformed.chrnames.csv | awk 'BEGIN {OFS="\t"} NR==1 {print "CHROM", "BEG", "END"; next} {print $1, $2-12500, $2+12500}' > /xdisk/mcnew/dannyjackson/cardinals/analyses/fst/nocaurban_nocarural/nocaurban_nocarural.fst.25000.outlier.regionsfile.tsv
-
-bcftools view [OPTIONS] file.vcf.gz [REGION […​]]
-
-
-bcftools view /xdisk/mcnew/dannyjackson/cardinals/datafiles/genotype_calls/cardinals_snps_multiallelic.vcf -R /xdisk/mcnew/dannyjackson/cardinals/analyses/fst/nocaurban_nocarural/nocaurban_nocarural.fst.25000.outlier.regionsfile.tsv -o nocaurban_nocarural.fst.25000.outliers.vcf
-
-# ~/programs/jdk-23.0.2/bin/java -jar snpEff.jar download GRCg6a.105
-~/programs/jdk-23.0.2/bin/java -Xmx20g -jar snpEff.jar build -v b10k 
-
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gtf22 -v b10k -noCheckProtein
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gtf22 -v b10k
-~/programs/jdk-23.0.2/bin/java -Xmx8g -jar snpEff.jar -v b10k VYXE01005221.vcf > VYXE01005221.ann.vcf
-~/programs/jdk-23.0.2/bin/java -Xmx8g -jar snpEff.jar -v b10k noca.filtered.geno25.maf1.vcf > noca.filtered.geno25.maf1.ann.vcf
-~/programs/jdk-23.0.2/bin/java -Xmx8g -jar snpEff.jar -v b10k pyrr.filtered.geno25.maf1.vcf > pyrr.filtered.geno25.maf1.ann.vcf
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gff3 -v ncbi_cardinalis 
-~/programs/jdk-23.0.2/bin/java -Xmx4g -jar /path/to/snpEff/snpEff.jar -c /path/to/snpEff/snpEff.config -v b10k VYXE01005221.vcf > VYXE01005221.an…
-https://snpeff.blob.core.windows.net/databases/v5_1/snpEff_v5_1_b10k.zip
-. File '/Users/danjack/snpEff/./data/b10k/genes.gtf' line 33	'VYXE01000004.1	Genbank	CDS	19854	19961	.	-	0	gene_id	"CAR…
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gtf22 -v b10k
-~/programs/jdk-23.0.2/bin/java -jar snpEff.jar build -gtf22 -v b10k -noCheckProtein
+sbatch --account=mcnew \
+        --job-name=snpeff \
+        --partition=standard \
+        --mail-type=ALL \
+        --output=slurm_output/output.snpeff.%j \
+        --nodes=1 \
+        --ntasks-per-node=4 \
+        --time=5:00:00 \
+        --mem=50gb \
+        /xdisk/mcnew/dannyjackson/cardinals/scripts/snpeff_filtered.sh
