e1302f986c15814fd0485dfff3959eaeb2554d05
Danny Jackson <dnjacks4@asu.edu>
2025-04-18 15:37:20 -0700

    Update manhattanplot.filteredfiles.tajimadiff.r

--- general_scripts/manhattanplot.filteredfiles.tajimadiff.r
+++ general_scripts/manhattanplot.filteredfiles.tajimadiff.r
@@ -122,7 +122,8 @@ ggplot(plot_data, aes(x = BPcum, y = !!sym(metric))) +
     panel.grid.minor.x = element_blank()
   )
 
-ggsave(filename = "${input}.sigline.png", 
+
+ggsave(filename = paste0(input, ".sigline.png", 
        width = 20, height = 5, units = "in")
 
 cat("Script completed successfully!\n")
