8654ebadb411b7abc2bf9abf13d2d27b3e7d92d6
ljvossler <ljvossler@arizona.edu>
2025-08-05 14:38:39 -0700

    updates to msmc scripts

--- B_Phylogenetics/msmc/msmc_3_runMSMC.sh
+++ B_Phylogenetics/msmc/msmc_3_runMSMC.sh
@@ -15,18 +15,15 @@ if [ $# -lt 1 ]; then
     echo "  -p   Path to parameter file (example in GitHub repository as params.sh)"
     echo "  -m   File Name of your unique project msmc params file"
     echo "  -i   Individual or Population Name"
-    echo "  -t   Either "data" or "bootstraps". Doesn't change how MSMC is run, just determines where to look for input files and such. 
-                        (Wanted single script to run MSMC from. If developing this workflow from the start, it would be implemented cleaner, but this works for now during testing)"
     exit 1
 fi
 
 # Parse command-line arguments
-while getopts "pmit" option; do
+while getopts "pmi" option; do
     case "${option}" in
         p) PARAMS=${OPTARG} ;;
         m) MSMCPARAMS=${OPTARG} ;;
         i) POP_OR_IND=${OPTARG} ;;
-        t) RUN_TYPE=${OPTARG} ;;
         *) echo "Invalid option: -$OPTARG" >&4; exit 1 ;;
     esac
 done
@@ -51,9 +48,27 @@ source "${SCRIPTDIR}/${MSMCPARAMS}"
 RUN_NAME=msmc_${POP_OR_IND}_${DATE}
 echo ${RUN_NAME}
 
+
+if [ $NR_IND == 1 ]; then
+
+	find ${MSMCDIR}/input/msmc_input.${POP_OR_IND}.*.txt -size 0 -delete
+	ls ${MSMCDIR}/input/msmc_input.${POP_OR_IND}.*.txt | grep -v $sex_chr > ${OUTDIR}/input/SCAFS_INPUT_${POP_OR_IND}
+else
+	for i in `cat /xdisk/mcnew/finches/ljvossler/finches/${POP_OR_IND}_IND.txt`
+       do echo $i
+       IND=$i
+
+       for s in `cat /xdisk/mcnew/finches/ljvossler/finches/SCAFFOLDS.txt`
+               do echo $s
+               ls ${MSMCDIR}/input/msmc_input.${IND}.${s}.txt >> ${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}
+      done
+  done
+fi
+
+
 ### Report settings/parameters:
 date
-echo "Script: run_MSMC.sh"
+echo "Script: msmc_3_runMSMC.sh"
 echo "Run name: $RUN_NAME"
 echo "SNP calling method: $METHOD"
 echo "Period setting: $P_PAR"
@@ -61,75 +76,44 @@ echo "Nr of individuals (1 or 2+): $NR_IND"
 echo "Population or individuals ID: $POP_OR_IND"
 echo "Individual: "
 echo "Scaffolds: SCAFS_INPUT_${POP_OR_IND}"
-echo "Iterations: $NUM_OPT"
+echo "Iterations: ${NUM_OPT}"
 
 
-if [ $RUN_TYPE == data ]; then
+if [ $NR_IND == 1 ]
+        then
+        echo "Running MSMC for one individual"
+        MSMC_INPUT=`cat ${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}`
+        MSMC_OUTPUT=${MSMCDIR}/output/msmc_output.${RUN_NAME}
 
-                if [ $NR_IND == 1 ]; then
-
-                        find ${MSMCDIR}/input/msmc_input.${POP_OR_IND}.*.txt -size 0 -delete
-                        ls ${MSMCDIR}/input/msmc_input.${POP_OR_IND}.*.txt | grep -v $sex_chr > ${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}
+        if [ -f "${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}" ]
+                then
+                        echo "MSMC_INPUTS: SCAFS_INPUT_${POP_OR_IND}_noLG9"
+                        echo "MSMC_OUTPUT: $MSMC_OUTPUT"
                 else
-                        for i in `cat ${POP_OR_IND}_IND.txt`
-                do echo $i
-                IND=$i
+                        echo "MSMC_INPUT does not exist! Exiting now"
+                        exit 1
+        fi
 
-                for s in `cat SCAFFOLDS.txt`
-                        do echo $s
-                        ls ${MSMCDIR}/input/msmc_input.${IND}.${s}.txt >> ${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}
-                done
-                done
-                fi
-
-
-                if [ $NR_IND == 1 ]
-                        then
-                        echo "Running MSMC for one individual"
-                        MSMC_INPUT=`cat ${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}`
-                        MSMC_OUTPUT=${MSMCDIR}/output/msmc_output.${RUN_NAME}
-
-                        if [ -f "${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}" ]
-                                then
-                                        echo "MSMC_INPUTS: SCAFS_INPUT_${POP_OR_IND}_noLG9"
-                                        echo "MSMC_OUTPUT: $MSMC_OUTPUT"
-                                else
-                                        echo "MSMC_INPUT does not exist! Exiting now"
-                                        exit 1
-                        fi
-
-                        msmc2_Linux -t $THREADS -p $P_PAR -i $NUM_OPT -o $MSMC_OUTPUT -I 0,1 $MSMC_INPUT
-
-                        mv $MSMC_OUTPUT*loop.txt ${MSMCDIR}/output/log_and_loop/
-                        mv $MSMC_OUTPUT*log ${MSMCDIR}/output/log_and_loop/
-                else
-                        echo "Running MSMC for $NR_IND individuals"
-                        MSMC_INPUT=`cat ${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}`
-                        MSMC_OUTPUT=${MSMCDIR}/output/msmc_output.${POP_OR_IND}.${RUN_NAME}
-                        
-                        msmc2_Linux -t $THREADS -p $P_PAR -i $NUM_OPT -o ${MSMC_OUTPUT} -I `echo $INDEX` $MSMC_INPUT
-
-                        mv $MSMC_OUTPUT*loop.txt ${MSMCDIR}/output/log_and_loop/
-                        mv $MSMC_OUTPUT*log ${MSMCDIR}/output/log_and_loop/
-                fi
+        INDEX=0,1
 
 else
-                for x in `cat ${MSMCDIR}/bs_file_lists/${POP_OR_IND}.bs_file_list.txt`; do
-                        BS_INPUT_DIR=${MSMCDIR}/bootstrap/$x
-                        MSMC_BS=$(find ${MSMCDIR}/bootstrap/$x -maxdepth 2 -name "bootstrap_multihetsep*.txt")
-                        echo $MSMC_BS
-                        
-                        MSMC_OUTPUT=${MSMCDIR}/bootstrap/outputs/${POP_OR_IND}/msmc_output.$x
-                        echo $MSMC_OUTPUT
-                        
-                        echo "running msmc2 on bootstraps for $x"
-                        msmc2_Linux -t $THREADS i- $NUM_OPT -o $MSMC_OUTPUT $MSMC_BS
-                        
-                        mv $MSMC_OUTPUT*loop.txt ${MSMCDIR}/bootstrap/outputs/${POP_OR_IND}/log_and_loop/
-                        mv $MSMC_OUTPUT*log ${MSMCDIR}/bootstrap/outputs/${POP_OR_IND}/log_and_loop/
-
+        echo "Running MSMC for $NR_IND individuals"
+        MSMC_INPUT=`cat ${MSMCDIR}/input/SCAFS_INPUT_${POP_OR_IND}`
+        MSMC_OUTPUT=${MSMCDIR}/output_2/msmc_output.${POP_OR_IND}.${RUN_NAME}
+        
+       	NR_HAPS=`expr $NR_IND \* 2`
+       	INDEX=$(for num in `seq 0 $(expr ${NR_HAPS} - 1)`; do echo -n "${num},"; done)
+       	INDEX=${INDEX::-1}
 
 fi
+
+
+# Run MSMC now that all necessary params are set
+msmc2_Linux -t $THREADS -p $P_PAR -i $NUM_OPT -o ${MSMC_OUTPUT} -I `echo $INDEX` $MSMC_INPUT 
+
+mv $MSMC_OUTPUT*loop.txt ${MSMCDIR}/output/log_and_loop/
+mv $MSMC_OUTPUT*log ${MSMCDIR}/output/log_and_loop/
+
 
 echo "done running msmc2 for ${POP_OR_IND}"
 date
