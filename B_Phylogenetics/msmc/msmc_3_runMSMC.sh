fcb240e15a529e44e3c1eb0ed2039b9bb4b3e8a6
ljvossler <ljvossler@arizona.edu>
2025-08-08 13:46:35 -0700

    fixes

--- B_Phylogenetics/msmc/msmc_3_runMSMC.sh
+++ B_Phylogenetics/msmc/msmc_3_runMSMC.sh
@@ -51,10 +51,11 @@ if [ $NR_IND == 1 ]; then
 	find ${MSMCDIR}/input/msmc_input.${POP_OR_IND}.*.txt -size 0 -delete
 	ls ${MSMCDIR}/input/msmc_input.${POP_OR_IND}.*.txt | grep -v $sex_chr > ${OUTDIR}/input/SCAFS_INPUT_${POP_OR_IND}
 else
-
+    echo "" > ${MSMCDIR}/multi_indv_data/input/SCAFS_INPUT_${POP_OR_IND}
+    
     for s in `cat /xdisk/mcnew/finches/ljvossler/finches/SCAFFOLDS.txt`
         do echo $s
-        ls ${MSMCDIR}/multi_indv_data/input/msmc_input.${POP_OR_IND}.${s}.txt >> ${MSMCDIR}/multi_indv_data/input/SCAFS_INPUT_${POP_OR_IND}
+        ls ${MSMCDIR}/multi_indv_data/input/msmc_input.${POP_OR_IND}.${s}.txt > ${MSMCDIR}/multi_indv_data/input/SCAFS_INPUT_${POP_OR_IND}
     done
 fi
 
