13e43376157691103f3d40aed44997f3b4ccb30f
ljvossler <ljvossler@arizona.edu>
2025-12-03 10:32:44 -0700

    testing gone script updates/fixes

--- B_Phylogenetics/gone/input_file_generation.sh
+++ B_Phylogenetics/gone/input_file_generation.sh
@@ -10,16 +10,20 @@ VCFFILE=$1
 SCAFFOLDS=$2
 OUTPREFIX=$3
 
-mkdir $OUTPREFIX
-mkdir $OUTPREFIX/gone_input
+if [ ! -d "$OUTPREFIX/gone_input" ]; then
+  echo "Directory for gone_input does not exist. Creating it now..."
+  mkdir -p "$OUTPREFIX/gone_input" # -p creates parent directories if they don't exist
+else
+  echo "Directory for gone_input already exists."
+fi
 
 # Convert VCF to proper .map file format for GONE
-vcftools --gzvcf $VCFFILE --plink --chrom-map $SCAFFOLDS --out $OUTPREFIX
+vcftools --gzvcf $VCFFILE --plink --chrom-map $SCAFFOLDS --remove-indels --out $OUTPREFIX
 rm $OUTPREFIX.ped
 mv $OUTPREFIX.map $OUTPREFIX/gone_input
 
 # Convert VCF to proper .binary file format
-plink --vcf $VCFFILE --make-bed --allow-extra-chr --out $OUTPREFIX
+plink --vcf $VCFFILE --make-bed --allow-extra-chr --snps-only --out $OUTPREFIX
 # Convert binary file format to text .ped file format
 plink --bfile $OUTPREFIX --recode --allow-extra-chr --out $OUTPREFIX
 mv $OUTPREFIX.ped $OUTPREFIX/gone_input
--- B_Phylogenetics/gone/submit_gone.sh
+++ B_Phylogenetics/gone/submit_gone.sh
@@ -8,6 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=01:00:00
+#SBATCH --ntasks-per-node=4
 ##SBATCH --gres=gpu:1
 #SBATCH --output run_gone_cra_post.out
 ##SBATCH --constraint=hi_mem
@@ -36,13 +37,23 @@ rm $POPNAME.ped
 rm -r TEMPORARY_FILES
 
 cd ../../
-mkdir $POPNAME/gone_output
-mv $GONEDIR/outfileHWD $POPNAME/gone_output/
-mv $GONEDIR/Output_d2_$POPNAME $POPNAME/gone_output/
-mv $GONEDIR/Output_Ne_$POPNAME $POPNAME/gone_output/
-mv $GONEDIR/OUTPUT_$POPNAME $POPNAME/gone_output/
-mv $GONEDIR/timefile $POPNAME/gone_output/
-mv $GONEDIR/seedfile $POPNAME/gone_output/
+
+OUTDIR=$POPNAME/gone_output
+
+if [ ! -d $OUTDIR ]; then
+  echo "Output directory for $POPNAME does not exist. Creating it now..."
+  mkdir -p $OUTDIR
+else
+  echo "Output directory for $POPNAME already exists."
+fi
+
+
+mv $GONEDIR/outfileHWD $OUTDIR/
+mv $GONEDIR/Output_d2_$POPNAME $OUTDIR/
+mv $GONEDIR/Output_Ne_$POPNAME $OUTDIR/
+mv $GONEDIR/OUTPUT_$POPNAME $OUTDIR/
+mv $GONEDIR/timefile $OUTDIR/
+mv $GONEDIR/seedfile $OUTDIR/
 
 echo "Completed GONE Analysis for $POPNAME"
  
