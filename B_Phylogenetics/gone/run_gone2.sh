c33f53f828e9f64d5109a368761f5cc6b56e2a66
ljvossler <ljvossler@arizona.edu>
2026-01-15 13:12:13 -0700

    building batch arry

---
+++ B_Phylogenetics/gone/run_gone2.sh
@@ -0,0 +1,14 @@
+echo "Running GONE for $OUTPREFIX with $NUMIND individuals with recombination rate of $RECOMB_RATE..."
+cd GONE2/
+./gone2 $INDIR/$OUTPREFIX.ped -g $GENO_DTYPE -r $RECOMB_RATE -i $NUMIND -t 4 -o $OUTPREFIX $EXTRA_FLAGS
+
+
+echo "Organizing Output Files..."
+
+mv ${OUTPREFIX}_GONE2_d2 $OUTDIR/
+mv ${OUTPREFIX}_GONE2_Ne $OUTDIR/
+mv ${OUTPREFIX}_GONE2_STATS $OUTDIR/
+
+
+echo "Completed GONE Analysis for $OUTPREFIX"
+ 
