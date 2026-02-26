0fb95e4423fecb576b1ea9c4679cc9da37e15c0b
ljvossler <ljvossler@arizona.edu>
2026-02-26 15:11:18 -0700

    testing SFS script

--- A_Preprocessing/A1.3_siteallelefrequency.sh
+++ A_Preprocessing/A1.3_siteallelefrequency.sh
@@ -5,17 +5,22 @@
 if [ $# -lt 1 ]
   then
     echo "This script computes the genome-wide site allele frequency spectrum within a population of genomes using a genotype likelihood framework implemented in ANGSD. It requires a list of pre-processed bam files, the scripts for which are A1-A4 in this repository. 
+    You should make your population bam list on your own and save them in the format POP_bams.txt in the referencelists directory.
 
     This is an essential step for analyzing fst, dxy, and Tajima's D.
     
     REQUIRED ARGUMENTS
-    [-p] Path to parameter file (example is saved in the GitHub repository as params.sh)."
+    [-p] Path to parameter file (example is saved in the GitHub repository as params.sh).
+    -n Name of population 
+    -m Path to headless MAF file generated in A1.1_snpID.sh"
+
   else
-    while getopts p:n: option
+    while getopts p:n:m: option
     do
     case "${option}" in
     p) PARAMS=${OPTARG};;
     n) POP=${OPTARG};;
+    m) MAFFILE=${OPTARG};;
     esac
     done
 
@@ -27,7 +32,7 @@ source "${PARAMS}"
 
 printf "\n \n \n \n"
 date
-echo "Current script: fst.sh"
+echo "Current script: A1.3_siteallelefrequency.sh"
 
 
 # Generate SAF files for each population in ANGSD. Skip if output already exists.
@@ -36,8 +41,9 @@ if [ -f "${OUTDIR}/datafiles/safs/${POP}"* ]
         then
             echo "${POP} files present in SAFs directory, assuming they are already generated and moving on!"
         else
-            echo "Computing SAFs for population 1"
-            ${ANGSD}/angsd -bam ${OUTDIR}/referencelists/${POP}bams.txt -out ${OUTDIR}/datafiles/safs/${POP} -dosaf 1 -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 3 -doCounts 1 -doDepth 1 -setMinDepthInd ${MINDEPTHIND} -minInd ${MININD} -minQ ${MINQ} -minMapQ ${MINMAPQ} -sites ${OUTDIR}/referencelists/allsnps.sites_headless.mafs -anc ${REF} -nThreads ${THREADS} 
+            echo "Computing SAFs for ${POP}"
+            ${ANGSD}/angsd -bam ${OUTDIR}/referencelists/${POP}_bams.txt -out ${OUTDIR}/datafiles/safs/${POP} -dosaf 1 -GL 1 -doGlf 2 -doMaf 1 -doMajorMinor 3 -doCounts 1 -doDepth 1 \
+            -setMinDepthInd ${MINDEPTHIND} -minInd ${MININD} -minQ ${MINQ} -minMapQ ${MINMAPQ} -sites ${MAFFILE} -anc ${REF} -nThreads ${THREADS} 
 fi
 
 
