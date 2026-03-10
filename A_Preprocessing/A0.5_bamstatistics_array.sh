5867e5a45462d04c9025c76ab289e096ebbc1185
ljvossler <ljvossler@arizona.edu>
2026-03-10 12:01:28 -0700

    updated bamstats array to be more consistent with other scripts

--- A_Preprocessing/A0.5_bamstatistics_array.sh
+++ A_Preprocessing/A0.5_bamstatistics_array.sh
@@ -8,7 +8,7 @@ This script computes average depth statistics of a bam file. Meant to be used in
 Required argument:
   -p  Path to the parameter file (e.g., params_preprocessing.sh in the GitHub repository).
   -r  Run name, required for providing a unique name to output files.
-  -s  Sample name
+  -i  Individual name (can easily be passed through a slurm array).
   -c  Optional flag to indicate whether to compress the output files with bgzip."
     exit 1
 fi
@@ -17,11 +17,11 @@ fi
 COMPRESS=false
 
 # Parse command-line arguments
-while getopts p:r:s:c option; do
+while getopts p:r:i:c option; do
     case "${option}" in
         p) PARAMS=${OPTARG};;
         r) RUNNAME=${OPTARG};;
-        s) SAMPLE=${OPTARG};;
+        i) IND=${OPTARG};;
         c) COMPRESS=true ;;
         *) echo "Invalid option: -${OPTARG}" >&2; exit 1;;
     esac
@@ -46,27 +46,27 @@ if [ -z "$OUTDIR" ] ; then
 fi
 
 ## Compute statistics on bam file
-echo "Current bam: $SAMPLE"
-echo $SAMPLE >> ${OUTDIR}/datafiles/bamstats/"$SAMPLE"_depthstats.txt 
-samtools depth ${BAMDIR}/"$SAMPLE".realigned.bam >> ${OUTDIR}/datafiles/bamstats/"$SAMPLE"_depthstats.txt 
-echo "Finished: $SAMPLE"
+echo "Current bam: $IND"
+echo $IND >> ${OUTDIR}/datafiles/bamstats/"$IND"_depthstats.txt 
+samtools depth ${BAMDIR}/"$IND".realigned.bam >> ${OUTDIR}/datafiles/bamstats/"$IND"_depthstats.txt 
+echo "Finished: $IND"
 
 
 # Save summary stats in CSV format
-echo "Generating summary bamstats for $SAMPLE"
+echo "Generating summary bamstats for $IND"
 # Compute average and standard deviation
-stats=$(awk '{sum+=$3; sumsq+=$3*$3} END { print sum/NR "," sqrt(sumsq/NR - (sum/NR)**2) }' ${OUTDIR}/datafiles/bamstats/"$SAMPLE"_depthstats.txt)
+stats=$(awk '{sum+=$3; sumsq+=$3*$3} END { print sum/NR "," sqrt(sumsq/NR - (sum/NR)**2) }' ${OUTDIR}/datafiles/bamstats/"$IND"_depthstats.txt)
 
 if [[ -f "${OUTDIR}/datafiles/bamstats/${RUNNAME}.depthstats.txt" ]]; then
-    echo "$SAMPLE,$stats" >> ${OUTDIR}/datafiles/bamstats/${RUNNAME}.depthstats.txt
+    echo "$IND,$stats" >> ${OUTDIR}/datafiles/bamstats/${RUNNAME}.depthstats.txt
 else
     echo "Sample,Average,Stdev" > ${OUTDIR}/datafiles/bamstats/${RUNNAME}.depthstats.txt
-    echo "$SAMPLE,$stats" >> ${OUTDIR}/datafiles/bamstats/${RUNNAME}.depthstats.txt
+    echo "$IND,$stats" >> ${OUTDIR}/datafiles/bamstats/${RUNNAME}.depthstats.txt
 fi
 
 
 # Since the sample bamstats files are really large, let's compress them for now while we may need them in the near future.
 if [ "$COMPRESS" = true ]; then
-    echo "Compressing ${SAMPLE} depthstats file"
-    bgzip "${OUTDIR}/datafiles/bamstats/${SAMPLE}_depthstats.txt"
+    echo "Compressing ${IND} depthstats file"
+    bgzip "${OUTDIR}/datafiles/bamstats/${IND}_depthstats.txt"
 fi
