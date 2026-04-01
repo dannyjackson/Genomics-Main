097b25b3bd11c082cf5eaef5032422572286e0b7
ljvossler <ljvossler@arizona.edu>
2026-04-01 13:14:47 -0700

    progress on shapeit and sapphire script (untested)

--- A_Preprocessing/A2.5.2_phasing_shapeit.sh
+++ A_Preprocessing/A2.5.2_phasing_shapeit.sh
@@ -91,24 +91,75 @@ while read -r SCAFFOLD; do
     # Perform sapphire polishing
 
     if [ "$SAPPHIRE" = true ]; then
-        echo "Polishing phased VCF for scaffold $SCAFFOLD with SAPPHIRE..."
-        sapphire phase --bam "${BAMDIR}/${IND}.realigned.bam" --vcf "$VCF_OUT" --out "${OUTDIR}/datafiles/split_vcf/${POP}_${SCAFFOLD}.sapphire.vcf.gz" --threads $THREADS
-        
-        if [[ $? -ne 0 ]]; then
-            echo "Error: SAPPHIRE polishing failed for scaffold $SCAFFOLD."
-            exit 1
+
+        if [ -d "${OUTDIR}/datafiles/sapphire_polishing" ]; then
+        echo "Directory ${OUTDIR}/datafiles/sapphire_polishing exists."
+        else
+        echo "Directory ${OUTDIR}/datafiles/sapphire_polishing does not exist Creating it now."
+        mkdir -p ${OUTDIR}/datafiles/sapphire_polishing
         fi
+
+        echo "Polishing phased VCF for scaffold $SCAFFOLD with SAPPHIRE..."
+
+        VCF_ANNOTATED="${OUTDIR}/datafiles/split_vcf/${POP}_${SCAFFOLD}.phased_PP_annotated.bcf"
+        VCF_REPHASED="${OUTDIR}/datafiles/split_vcf/${POP}_${SCAFFOLD}.rephased.bcf"
+        EXTRACTED_PP="${OUTDIR}/datafiles/sapphire_polishing/${POP}_${SCAFFOLD}.phased_PP_extract.bin"
+        EXTRACTED_PP_ORIGINAL="${OUTDIR}/datafiles/sapphire_polishing/${POP}_${SCAFFOLD}.phased_PP_extract.bin.original"
+        ANNOTATED_CSV="${OUTDIR}/datafiles/sapphire_polishing/${POP}_${SCAFFOLD}.phased_PP_annotated.samples.csv"
+        PP_INFO="${OUTDIR}/datafiles/sapphire_polishing/pp_info.tsv"
+        HEADER="${OUTDIR}/sapphire_polishing/header.txt"
+
+        # Extract all variants with AF < 0.01 and replace heterozygous by "0.5" and homozygous by "."
+        bcftools filter -i 'INFO/AF<0.01' $VCF_OUT -Ou | bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t[%GT\t]\n' | sed -e 's/0|0/./g' -e 's/0|1/0.5/g' -e 's/1|0/0.5/g' -e 's/1|1/./g' > $PP_INFO
+        # bgzip and tabix annotate the file
+        bgzip $PP_INFO
+        tabix -s1 -b2 -e2 $PP_INFO.gz
+
+        if [ -f "${HEADER}" ];
+            then
+                echo "Header file already exists, moving on!"
+            else
+                echo '##FORMAT=<ID=PP,Number=1,Type=Float,Description="SHAPEIT Phasing Probability (PP) field">' > ${HEADER}
+        fi
+
+
+        bcftools annotate -a ${PP_INFO}.gz -h ${HEADER} -c CHROM,POS,ID,REF,ALT,FORMAT/PP \
+        $VCF_OUT -Ob -o $VCF_ANNOTATED
+
+        # Sanity check the annotated VCF
+        bcftools view "$VCF_ANNOTATED" | less
+
+        ./sapphire/pp_extractor/pp_extract -f $VCF_ANNOTATED --show-number -o $EXTRACTED_PP
+        # Copy the file as SAPPHIRE will modify it, with this we will be able to compare
+        cp "$EXTRACTED_PP" "$EXTRACTED_PP_ORIGINAL"
+
+        bcftools query --list-samples $VCF_ANNOTATED | \
+        awk '{print NR-1 "," $0 ",CRAM_PATH"}' > \
+        $ANNOTATED_CSV
+
+        ./sapphire/phase_caller/phase_caller \
+        -f $VCF_ANNOTATED \
+        -S $ANNOTATED_CSV \
+        --cram-path-from-samples-file \
+        -b "$EXTRACTED_PP" \
+        -t $(nproc)
+
+        #./sapphire/bin_tools/bin_diff -a "$EXTRACTED_PP_ORIGINAL" \
+        #-b "$EXTRACTED_PP" \
+        #-f $VCF_OUT \
+        #-S $ANNOTATED_CSV \
+        #--extra-info --more | less
+
+        ./sapphire/pp_update/pp_update -f $VCF_ANNOTATED \
+        -b $EXTRACTED_PP \
+        -o $VCF_REPHASED
+
+
         
         #Remove the unpolished phased VCF
         rm "$VCF_OUT"
         
     fi
-    # Ensure BAM file exists
-    #BAMFILE="${BAMDIR}/${IND}.realigned.bam"
-    #if [[ ! -f "$BAMFILE" ]]; then
-    #    echo "Error: BAM file for $IND not found in $BAMDIR. Exiting."
-    #    exit 1
-    #fi
 
 
 
