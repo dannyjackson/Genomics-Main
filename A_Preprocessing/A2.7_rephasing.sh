8adc9145ba6395719111531b6327fae34950e18a
ljvossler <ljvossler@arizona.edu>
2026-04-27 14:28:44 -0700

    rephasing fix. moved fill-tags into filtering script

--- A_Preprocessing/A2.7_rephasing.sh
+++ A_Preprocessing/A2.7_rephasing.sh
@@ -69,7 +69,7 @@ HEADER="${REPHASE_DIR}/header.txt"
 CRAM_PATH="${OUTDIR}/datafiles/indelrealignment/" # Should be a directory containing all individual CRAM files
 
 # Extract all variants with AF < 0.01 and replace heterozygous by "0.5" and homozygous by "."
-bcftools filter -i 'INFO/AF < 0.01' $BCF -Ou | bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t[%GT\t]\n' | sed -e 's/0|0/./g' -e 's/0|1/0.5/g' -e 's/1|0/0.5/g' -e 's/1|1/./g' > $PP_INFO
+bcftools filter -i 'INFO/AF < 0.2' $BCF -Ou | bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t[%GT\t]\n' | sed -e 's/0|0/./g' -e 's/0|1/0.5/g' -e 's/1|0/0.5/g' -e 's/1|1/./g' > $PP_INFO
 # bgzip and tabix annotate the file
 bgzip $PP_INFO
 tabix -s1 -b2 -e2 $PP_INFO.gz
@@ -88,7 +88,7 @@ bcftools annotate -a ${PP_INFO}.gz -h ${HEADER} -c CHROM,POS,ID,REF,ALT,FORMAT/P
 
 
 echo "Extracting PP INFO"
-${PROGDIR}/sapphire/pp_extractor/pp_extract -f ${BCF_ANNOTATED} --pp-from-maf --maf-threshold 0.01 -o ${EXTRACTED_PP}
+${PROGDIR}/sapphire/pp_extractor/pp_extract -f ${BCF_ANNOTATED} --pp-from-maf --maf-threshold 0.2 -o ${EXTRACTED_PP}
 # Copy the file as SAPPHIRE will modify it, with this we will be able to compare
 cp "$EXTRACTED_PP" "${EXTRACTED_PP}.original"
 
