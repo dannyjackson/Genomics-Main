36c75ebe9c54a22ce285912dde713e919ad54bdb
ljvossler <ljvossler@arizona.edu>
2026-07-27 09:27:34 -0700

    smc script updates

--- A_Preprocessing/A2.5.1_smc_demography.sh
+++ A_Preprocessing/A2.5.1_smc_demography.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 -p <parameter_file> -v <vcf_file> -s <sample_list_file> -i <population_prefix> [-d]
+    echo "Usage: $0 -p <parameter_file> -v <vcf_file> -s <sample_list_file> -i <population_prefix> [-d] -m <mask_file>
 
 This script generates demography estimates for a population using SMC++. This is a prerequisite for generating recombination maps using pyrho in A2.5.1_recombination_mapping.sh
 
@@ -11,20 +11,22 @@ Required argument:
   -s  Path to Population Sample List File
   -i  Population prefix
 Optional argument:
-  -d  Boolean argument specifying whether to convert a copy of SMC++ output to demes YAML file (Default to false). Needed if want to use downstream Flexsweep analysis"
+  -d  Boolean argument specifying whether to convert a copy of SMC++ output to demes YAML file (Default to false). Needed if want to use downstream Flexsweep analysis
+  -m  Path to a bgzipped and indexed bed file containing uncalled/lowcoverage regions not present in the VCF file (recommended)"
     exit 1
 fi
 
 DEMES=false
 
 # Parse command-line arguments
-while getopts ":p:v:s:i:d" option; do
+while getopts ":p:v:s:i:m:d" option; do
     case "${option}" in
         p) PARAMS=${OPTARG} ;;
         v) VCF=${OPTARG} ;;
         s) SAMPLES=${OPTARG} ;;
         i) POP=${OPTARG} ;;
         d) DEMES=true ;;
+        m) MASK=${OPTARG} ;;
         *) echo "Invalid option: -${OPTARG}" >&2; usage ;;
     esac
 done
@@ -48,29 +50,22 @@ if [ -d "${OUTDIR}/datafiles/demography/smc_inputs/${POP}" ];
             mkdir -p "${OUTDIR}/datafiles/demography/smc_inputs/${POP}"
 fi
 
-#bcftools index ${VCF}
+bcftools index ${VCF}
 # Check for bed mask file
-#if [ -f "${OUTDIR}/datafiles/mask/${POP}_smc_mask.bed.gz" ];
-#        then
-#            echo "bed mask file already exists, moving on!"
-#        else
-#            # Get chrom order of VCF to ensure order in scaffold lengths file matches
-#            zgrep -v '^#' ${VCF} | awk '{print $1}' | uniq > ${OUTDIR}/datafiles/genotype_calls/${POP}_vcf_chrom_order.txt
-#            awk 'FNR==NR {pos[$1]=NR; next} $1 in pos {print $0, pos[$1]}' vcf_chrom_order.txt ${OUTDIR}/referencelists/scaffold_lengths.txt \
-#            | sort -k3,3n \
-#            | awk '{print $1"\t"$2}' > ${OUTDIR}/referencelists/scaffold_lengths.vcf_sorted.txt
-#            # Generate bed file
-#            bedtools complement -i ${VCF}.sorted -g ${OUTDIR}/referencelists/scaffold_lengths.vcf_sorted.txt > ${OUTDIR}/datafiles/mask/${POP}_smc_mask.bed
-#            bgzip ${OUTDIR}/datafiles/mask/${POP}_smc_mask.bed
-#            tabix ${OUTDIR}/datafiles/mask/${POP}_smc_mask.bed.gz
-#fi
+if [[ -n "$MASK" ]]; then
+    echo "Mask File provided."
+    mask_flag="--mask ${MASK}"
+else
+    echo "No mask for uncalled regions provided. Proceeding without it, but note that you probably want a mask to account for uncalled regions."
+    mask_flag=""
+fi
 
 POPSAMPLES="$(cat ${SAMPLES} | paste -sd ",")"
 
 for chr in $(cat ${OUTDIR}/referencelists/SCAFFOLDS.txt);
 do
     echo "Processing $chr into SMC++ Input"
-    COMMAND="$VCF ${OUTDIR}/datafiles/demography/smc_inputs/${POP}/${POP}_${chr}.smc.gz $chr $POP:$POPSAMPLES --mask ${OUTDIR}/datafiles/mask/${POP}_sorted_mask.bed.gz"
+    COMMAND="$VCF ${OUTDIR}/datafiles/demography/smc_inputs/${POP}/${POP}_${chr}.smc.gz $chr $POP:$POPSAMPLES ${mask_flag}"
     apptainer run ${PROGDIR}/smcpp_latest.sif vcf2smc $COMMAND --ignore-missing # After VCF filtering, some samples may be thrown out. Passing --ignore-missing to proceed with smc without them and log the sample names
 done
 
